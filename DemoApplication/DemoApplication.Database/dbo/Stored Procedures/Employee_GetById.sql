889aee50beb389340897f8326200e5dda63cc32a
rodmjay <rodmjay@wcpro.com>
2013-05-23 04:02:24 -0600

    employee_ getbyid

--- DemoApplication/DemoApplication.Database/dbo/Stored Procedures/Employee_GetById.sql
+++ DemoApplication/DemoApplication.Database/dbo/Stored Procedures/Employee_GetById.sql
@@ -6,27 +6,18 @@
 -- Description:	Returns single employee with associated assignments
 -- =============================================
 CREATE PROCEDURE [dbo].[Employee_GetById] 
-	@Id char(30) 	
+	@EmployeeId char(30) 	
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 
-	SELECT dbo.Person_Main.Employee_Cd AS Id, 
-		   First_Name_Txt AS FirstName, 
-		   Last_Name_Txt AS LastName
-	FROM dbo.Person_Main 
-	WHERE Employee_Cd=@Id
+	SELECT 
+		*
+	FROM Employees
+	WHERE Id=@EmployeeId
 
-	SELECT  AssignmentId AS Id ,
-	        Name ,
-			DueDate ,
-			CompletedDate ,
-	        IsDone ,
-	        Employee_Cd AS EmployeeId ,
-	        --Principal_Type ,
-	        Principal_Cd AS AssigneeId
-	FROM dbo.Assignment
-	WHERE Employee_Cd=@Id
+	SELECT  * From Assignments
+	WHERE EmployeeId = @EmployeeId
 END
