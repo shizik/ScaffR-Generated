3bb72d1881e92568a18270a37789af6ba446ff11
rodmjay <rodmjay@wcpro.com>
2013-05-23 04:22:19 -0600

    [Principal_GetAvailable]

--- DemoApplication/DemoApplication.Database/dbo/Stored Procedures/Principal_GetAvailable.sql
+++ DemoApplication/DemoApplication.Database/dbo/Stored Procedures/Principal_GetAvailable.sql
@@ -16,10 +16,6 @@ BEGIN
 
 	select @Company_Cd = Company_Cd from Person_Main p inner join Assignment a on p.Employee_Cd = a.Employee_Cd
 
-    select Employee_Cd as 'Principal_Cd', Last_Name_Txt + ',' + First_Name_Txt as 'Name', 0 as 'IsTeam' from Person_Main where Company_Cd = @Company_Cd
-
-	union
-
-	Select Team_Cd as 'Principal_Cd', Name, 1 as 'IsTeam' from Team where Company_Cd = @Company_Cd
+    select * from Principals where CompanyId = @Company_Cd
 
 END
--- DemoApplication/DemoApplication.Database/dbo/Views/Principals.sql
+++ DemoApplication/DemoApplication.Database/dbo/Views/Principals.sql
@@ -1,7 +1,7 @@
 ﻿CREATE VIEW [dbo].[Principals]
 	AS 
- select Employee_Cd as 'PrincipalId', Last_Name_Txt + ',' + First_Name_Txt as 'Name', CAST(0 as Bit) as 'IsTeam' from Person_Main
+ select Employee_Cd as 'PrincipalId', Last_Name_Txt + ',' + First_Name_Txt as 'Name', CAST(0 as Bit) as 'IsTeam', Company_Cd as 'CompanyId' from Person_Main
 
 	union
 
-	Select Team_Cd as 'PrincipalId', Name, CAST(1 as BIT) as 'IsTeam' from Team
+	Select Team_Cd as 'PrincipalId', Name, CAST(1 as BIT) as 'IsTeam', Company_Cd as 'CompanyId' from Team
