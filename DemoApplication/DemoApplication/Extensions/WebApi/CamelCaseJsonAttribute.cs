2687c8a7e9356423cd4a91aa1e5bb93be1239549
Marko Ilievski <sk8mask@gmail.com>
2013-06-05 10:15:53 +0200

    Area or Controller specific CamelCase formatting

---
+++ DemoApplication/DemoApplication/Extensions/WebApi/CamelCaseJsonAttribute.cs
@@ -0,0 +1,44 @@
+﻿#region credits
+// ***********************************************************************
+// Assembly	: DemoApplication
+// Author	: Marko Ilievski
+// Created	: 06-05-2013
+// 
+// Last Modified By : Marko Ilievski
+// Last Modified On : 06-05-2013
+// ***********************************************************************
+#endregion
+
+namespace DemoApplication.Extensions.WebApi
+{
+    #region
+
+    using System;
+    using System.Linq;
+    using System.Net.Http.Formatting;
+    using System.Web.Http.Controllers;
+    using Newtonsoft.Json.Serialization;
+
+    #endregion
+
+    public class CamelCaseJsonAttribute : Attribute, IControllerConfiguration
+    {
+        public void Initialize(HttpControllerSettings config,
+                               HttpControllerDescriptor controllerDescriptor)
+        {
+            var formatter = config.Formatters.OfType<JsonMediaTypeFormatter>().SingleOrDefault();
+
+            if (formatter == null) return;
+
+            config.Formatters.Remove(formatter);
+
+            // We need to new this, so it doesn't overwrite global settings
+            formatter = new JsonMediaTypeFormatter
+                {
+                    SerializerSettings = { ContractResolver = new CamelCasePropertyNamesContractResolver() }
+                };
+
+            config.Formatters.Insert(0, formatter);
+        }
+    }
+}
